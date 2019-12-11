74209116f2ea381f3b1c5eceab5a6f3ed93dce3a
Aser <aser.ahmad@gmail.com>
2019-12-10 18:45:24 -0600

    Updated project artifactID and description. Updated UserRepository.

--- src/main/java/edu/mum/ea/socialnetwork/repository/UserRepository.java
+++ src/main/java/edu/mum/ea/socialnetwork/repository/UserRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    List<User> findUsersByEnabled(boolean enabled);
 }
