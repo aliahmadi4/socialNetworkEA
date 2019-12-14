25cc0e8b20a52014c7439e0cda54fc37fe5f784a
alimohammadahmadi <alimohammad.ahmadi2008@gmail.com>
2019-12-14 08:33:50 -0600

    showing posts of specific person in its profile page order by desc

--- src/main/java/edu/mum/ea/socialnetwork/repository/PostRepository.java
+++ src/main/java/edu/mum/ea/socialnetwork/repository/PostRepository.java
@@ -21,7 +21,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("update Post p set p.enabled= :isEnabled where p.id= :postId")
     void setPostEnabled(@Param("postId") Long postId, @Param("isEnabled") boolean isEnabled);
 
-    @Query("select p from Post p join p.user u where u.id = :id")
+    @Query("select p from Post p join p.user u where u.id = :id order by p.creationDate desc ")
     List<Post> findAllPostForSpecificUser(@Param("id") Long id);
 
 
